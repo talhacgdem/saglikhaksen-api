1096c7660191074cdaf450ef7de7b65d09465810
talhacgdem <talhacgdem@gmail.com>
2025-11-02 01:10:03 +0300

    subeler tekrar eklendi

--- config.php
+++ config.php
@@ -15,23 +15,24 @@ class Config
                 "password" => "W4r*krQyylnILj!Hglqd53vy",
             ],
             "contentTypes" => [
-    new ContentType(25, 'Duyuru & Gündem', 'duyurular', 'campaign', Types::category, true), // notifications -> campaign
-    new ContentType(29, 'Basında Biz', 'basinda-biz', 'newspaper', Types::category, true), // feed -> newspaper
-    new ContentType(28, 'Başvurularımız', 'basvurularimiz', 'assignment', Types::category, true), // notifications -> assignment
-    new ContentType(23, 'Haberler', 'haberler', 'article', Types::category, true), // article (zaten uygun)
-    new ContentType(30, 'Hedeflerimiz', 'hedeflerimiz', 'flag', Types::category, true), // notifications -> flag
-    new ContentType(0, 'Tekstil', 'saglik:tekstil', 'checkroom', Types::other, false), // subdirectory-arrow-right -> checkroom
-    new ContentType(1, 'Eğitim', 'saglik:egitim', 'school', Types::other, false), // subdirectory-arrow-right -> school
-    new ContentType(2, 'Hizmet', 'saglik:hizmet', 'build', Types::other, false), // subdirectory-arrow-right -> build
-    new ContentType(3, 'Eğlence', 'saglik:eglence', 'celebration', Types::other, false), // subdirectory-arrow-right -> celebration
-    new ContentType(4, 'Restaurant', 'saglik:restaurant', 'restaurant', Types::other, false), // subdirectory-arrow-right -> restaurant
-    new ContentType(5, 'Spor', 'saglik:spor', 'sports-soccer', Types::other, false), // subdirectory-arrow-right -> sports-soccer
-    new ContentType(6, 'Otomobil', 'saglik:otomobil', 'directions-car', Types::other, false), // subdirectory-arrow-right -> directions-car
-    new ContentType(7, 'Kuaför', 'saglik:kuafor', 'content-cut', Types::other, false), // subdirectory-arrow-right -> content-cut
-    new ContentType(8, 'Çiçek', 'saglik:cicek', 'local-florist', Types::other, false), // subdirectory-arrow-right -> local-florist
-    new ContentType(9, 'Kırtasiye', 'saglik:kirtasiye', 'edit', Types::other, false), // subdirectory-arrow-right -> edit
-    new ContentType(10, 'Tatil', 'saglik:tatil', 'beach-access', Types::other, false), // subdirectory-arrow-right -> beach-access
-]
+                new ContentType(25, 'Duyuru & Gündem', 'duyurular', 'campaign', Types::category, true),
+                new ContentType(29, 'Basında Biz', 'basinda-biz', 'newspaper', Types::category, true),
+                new ContentType(28, 'Başvurularımız', 'basvurularimiz', 'assignment', Types::category, true),
+                new ContentType(23, 'Haberler', 'haberler', 'article', Types::category, true),
+                new ContentType(30, 'Hedeflerimiz', 'hedeflerimiz', 'flag', Types::category, true),
+                new ContentType(0, 'Şubelerimiz', 'subelerimiz', 'business', Types::other, false),
+                new ContentType(1, 'Eğitim', 'saglik:egitim', 'school', Types::other, false),
+                new ContentType(2, 'Hizmet', 'saglik:hizmet', 'build', Types::other, false),
+                new ContentType(3, 'Eğlence', 'saglik:eglence', 'celebration', Types::other, false),
+                new ContentType(4, 'Restaurant', 'saglik:restaurant', 'restaurant', Types::other, false),
+                new ContentType(5, 'Spor', 'saglik:spor', 'sports-soccer', Types::other, false),
+                new ContentType(6, 'Otomobil', 'saglik:otomobil', 'directions-car', Types::other, false),
+                new ContentType(7, 'Kuaför', 'saglik:kuafor', 'content-cut', Types::other, false),
+                new ContentType(8, 'Çiçek', 'saglik:cicek', 'local-florist', Types::other, false),
+                new ContentType(9, 'Kırtasiye', 'saglik:kirtasiye', 'edit', Types::other, false),
+                new ContentType(10, 'Tatil', 'saglik:tatil', 'beach-access', Types::other, false),
+                new ContentType(11, 'Tekstil', 'saglik:tekstil', 'checkroom', Types::other, false),
+            ]
         ];
     }
 }
